59c21f8628ed9bf7227cd5567cb30f619391e3f7
Pranav Patel <pate0910@mylaurier.ca>
2020-04-04 15:11:17 -0400

    Initial Commit

---
+++ pate0910_a6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pate0910_a6
+//
+//  Created by Prism Student on 2020-04-04.
+//  Copyright © 2020 Pranav Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pate0910_a6Tests/pate0910_a6Tests.swift
@@ -0,0 +1,34 @@
+//
+//  pate0910_a6Tests.swift
+//  pate0910_a6Tests
+//
+//  Created by Prism Student on 2020-04-04.
+//  Copyright © 2020 Pranav Patel. All rights reserved.
+//
+
+import XCTest
+@testable import pate0910_a6
+
+class pate0910_a6Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
